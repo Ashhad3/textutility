80de29cb8e816ef3fbac5ab9316d7ed249921c1c
Ashhad3 <ashhadqureshi123@gmail.com>
2021-12-02 18:52:31 +0500

    Updates

---
+++ precache-manifest.60e1ac32643fb5c3b065ceaad2dcab51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70cf4a41186ca0f96eab1af15404357d",
+    "url": "/textutility/index.html"
+  },
+  {
+    "revision": "6d1098efb993b7201b67",
+    "url": "/textutility/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a6e8108a3064a3c3f67",
+    "url": "/textutility/static/js/2.cd0903fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/textutility/static/js/2.cd0903fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1098efb993b7201b67",
+    "url": "/textutility/static/js/main.4ceffb3e.chunk.js"
+  },
+  {
+    "revision": "362a04721ae092f1bb0e",
+    "url": "/textutility/static/js/runtime-main.a19df5a1.js"
+  }
+]);
